d7f2338928402864708ad9f735011454974cb952
Justin Vandehey <jpvandehey@gmail.com>
2014-05-02 23:42:34 -0400

    Added signup page

--- main.py
+++ main.py
@@ -20,12 +20,42 @@ import os
 import string
 import re
 import urllib2
+import random
+import hashlib
+import hmac
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+def get_user_api(username):
+    pass
+
+def hash_cookie(s):
+    h = hashlib.sha256(SECRET + s).hexdigest()
+    return s+'|'+str(h)
+
+def validate_pw(user_input, salt, pw):
+    return hashlib.sha256(user_input+salt).hexdigest() == pw
+
+def hash_pw(s):
+    salt = make_salt()
+    return hashlib.sha256(s+salt).hexdigest()+'|'+salt
+
+def make_salt():
+    r = ''
+    for i in range(5):
+        r += random.choice(string.letters)
+    return r
+
+def valid_password(password):
+    PASSWORD_RE = re.compile(r"^.{3,20}$")
+    return PASSWORD_RE.match(password)
+
+def verify_password(password, verify):
+    return password == verify
 
 #Generic Handler class
 class Handler(webapp2.RequestHandler):
@@ -47,8 +77,21 @@ class MainHandler(Handler):
     
     def get(self):
         self.render_main()
+        
+class SignupHandler(Handler):
+    def render_signup(self):
+        self.render("header.html")
+        self.render("signup.html")
     
+    def get(self):
+        self.render_signup()
+        
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
         
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/signup', SignupHandler,)
 ], debug=True)
